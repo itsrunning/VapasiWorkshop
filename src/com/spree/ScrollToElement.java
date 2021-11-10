eaf0da4c38dbb057e6fb373807d228a5efb6f03c
Dhivya-creator <dhivya.varadharajan@thoughtworks.com>
2021-11-10 20:36:55 +0530

    Adding scroll function

---
+++ src/com/spree/ScrollToElement.java
@@ -0,0 +1,25 @@
+package com.spree;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollToElement {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://spree-vapasi-prod.herokuapp.com/");
+//		Scroll into view
+		JavascriptExecutor je = (JavascriptExecutor) driver;
+		String jsCode = "arguments[0].scrollIntoView(true)";
+		String locator = "li[class='last next page-item']";
+		je.executeScript(jsCode,driver.findElement(By.cssSelector(locator)));
+		driver.close();
+		
+	}
+
+}
--- src/com/spree/ScrollUpScrollDown.java
+++ src/com/spree/ScrollUpScrollDown.java
@@ -5,20 +5,33 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ScrollJavaScript {
+public class ScrollUpScrollDown {
 
 	public static void main(String[] args) {
-
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://spree-vapasi-prod.herokuapp.com/");
-//		Scroll into view
+//		Scroll down
 		JavascriptExecutor je = (JavascriptExecutor) driver;
-		String jsCode = "arguments[0].scrollIntoView(true)";
-		String locator = "li[class='last next page-item']";
-		je.executeScript(jsCode,driver.findElement(By.cssSelector(locator)));
+		String scrollDown = "window.scrollBy(0,1135)";
+		je.executeScript(scrollDown);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+// Scroll up
+		String scrollUp = "window.scrollBy(0,-1135)";
+		je.executeScript(scrollUp);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		driver.close();
 	}
-
+	
 }
