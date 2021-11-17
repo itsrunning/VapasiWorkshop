4794b092b563776b0e361d11c97648147bcbff34
Gopishankar Haridas <gopishankarharidas@Gopishankars-MacBook-Pro.local>
2021-11-17 08:55:06 +0530

    :sparkles: add test for adding the product to cart

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -9,7 +9,6 @@ public class LoginPage {
     public void login(WebDriver driver, String userEmail, String password)
     {
         driver.findElement((By.linkText("Login"))).click();
-        //creating a new account, change email id everytime
         WebElement emailTextBox =  driver.findElement(By.id("spree_user_email"));
         emailTextBox.sendKeys(userEmail);
         WebElement passwordTextBox =  driver.findElement(By.name("spree_user[password]"));
--- src/test/java/TestsWithPageObjects/TestWebdriverAddItemToCartWithoutPageObject.java
+++ src/test/java/TestsWithPageObjects/TestWebdriverAddItemToCartWithoutPageObject.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestWebdriverLoginUserWithoutPageObject {
+public class TestWebdriverAddItemToCartWithoutPageObject {
 
 
     /* TEST 2 (Add Item to Cart)
@@ -29,26 +31,34 @@ public class TestWebdriverLoginUserWithoutPageObject {
         System.out.println("Before Test setting up chrome Driver");
         System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver");
         driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver,30000);
         driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
         driver.get("https://spree-vapasi-prod.herokuapp.com/");
         driver.manage().window().maximize();
 
+        // Register User
         login("test15nov11501@gmail.com","Password1");
 
         String welcomeMessageAlert = driver.findElement(By.cssSelector("div.alert.alert-success")).getText();
         Assert.assertTrue(welcomeMessageAlert.equals("Logged in successfully"));
+        driver.findElement(By.linkText("Bags")).click();
+        driver.findElement(By.xpath("//span[@title='Spree Bag']")).click();
+        driver.findElement(By.id("add-to-cart-button")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("update-cart")));
     }
 
 
     public void login(String userEmail, String password)
     {
         driver.findElement((By.linkText("Login"))).click();
+        //creating a new account, change email id everytime
         WebElement emailTextBox =  driver.findElement(By.id("spree_user_email"));
         emailTextBox.sendKeys(userEmail);
         WebElement passwordTextBox =  driver.findElement(By.name("spree_user[password]"));
         passwordTextBox.sendKeys(password);
         driver.findElement(By.name("commit")).click();
     }
+
 
     @AfterMethod
     public void rampDown()
